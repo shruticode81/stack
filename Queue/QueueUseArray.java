571bf2db6a5148bebd004cfb3fb95947b81f17e0
shruti anand <vitikaawesome81@gmail.com>
2021-03-27 16:30:51 +0530

    added DoubleCapacity(Dynamic Queue)

--- Queue/QueueUseArray.java
+++ Queue/QueueUseArray.java
@@ -1,11 +1,14 @@
 //package Queue;
 
 public class QueueUseArray {
-    public static void main(String[] args) throws QueueFullException{
-        QueueUsingArray queue= new QueueUsingArray();
+    public static void main(String[] args){
+        QueueUsingArray queue= new QueueUsingArray(3);
         queue.enqueue(5);
         queue.enqueue(6);
         queue.enqueue(8);
+        queue.enqueue(3);
+        queue.dequeue();
+        queue.enqueue(2);
         try {
             System.out.println(queue.front());
         } catch (QueueEmptyException e) {
--- Queue/QueueUsingArray.java
+++ Queue/QueueUsingArray.java
@@ -11,6 +11,12 @@ public class QueueUsingArray {
         rear=-1;
         size=0;
     }
+    public QueueUsingArray(int capacity){
+        data = new int[capacity];
+        front=-1;
+        rear=-1;
+        size=0;
+    }
     public int size(){
         return size;
     }
@@ -24,10 +30,11 @@ public class QueueUsingArray {
         }
         return data[front];
     }
-    public void enqueue(int element) throws QueueFullException{
+    public void enqueue(int element) {
         if(size==data.length){
             //throw QueueFullException
-            throw new QueueFullException();
+            //throw new QueueFullException();
+            doubleCapacity();
 
         }
         if(size==0){
@@ -43,6 +50,23 @@ public class QueueUsingArray {
         rear = (rear+1)%data.length;
         //rear++;
         data[rear]= element;
+    }
+    private void doubleCapacity(){
+        //call this func when queue is full
+        int temp[] = data;
+        data = new int[2*temp.length];
+        int index=0;
+        for(int i=front;i<temp.length;i++){
+            data[index]= temp[i];
+            index++;
+        }
+        for(int i=0;i<front;i++){
+            data[index]=temp[i];
+            index++;
+        }
+        front =0;
+        rear=size()-1;
+
     }
     public int dequeue(){
         int temp = data[front];
